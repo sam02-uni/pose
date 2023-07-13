a35ee9911777368541ef4caa1ec381823041377e
Pietro Braione <pietro.braione@gmail.com>
2023-07-13 15:32:40 +0200

    Change extraction to OCaml

--- Extraction.v
+++ Extraction.v
@@ -1,10 +1,13 @@
 From Coq Require Extraction.
-From Coq Require Import extraction.ExtrHaskellBasic.
-From Coq Require Import extraction.ExtrHaskellString.
-From Coq Require Import extraction.ExtrHaskellNatInteger.
+From Coq Require Import extraction.ExtrOcamlBasic.
+From Coq Require Import extraction.ExtrOcamlIntConv.
+From Coq Require Import extraction.ExtrOcamlNativeString.
 From POSE Require Import Syntax.
 From POSE Require Import Interp.
+From POSE Require Import Parse.
+From POSE Require Import Prettyprint.
+From POSE Require Import Smt.
 
-Extraction Language Haskell.
+Extraction Language OCaml.
 
-Extraction "./pose.hs" CObject compute step_at.
+Extraction "./pose.ml" CObject compute step_at parse step_to_str step_to_smt.
